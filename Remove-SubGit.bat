14269d45fcb9dd52eafb330268db396ff9559333
Kisella <kisella@foxmail.com>
2025-04-15 00:39:21 +0800

    rust_0406

--- Remove-SubGit.bat
+++ Remove-SubGit.bat
@@ -1,4 +1,4 @@
 @echo off
 powershell -Command "$currentDir = Get-Location; Get-ChildItem -Path $currentDir -Directory -Recurse -Filter '.git' -Force | Where-Object { $_.FullName -ne \"$currentDir\\.git\" } | ForEach-Object { Remove-Item -Path $_.FullName -Recurse -Force }"
-echo 所有子目录的 .git 已清理完成！
+echo .git of all subdirectories has been cleaned up! 
 pause
